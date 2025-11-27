d5f589a5347e2bd92868eba876971911a6f06983
Smrati Sharma <smrati.sharma@admiralindia.com>
2025-11-27 14:23:19 +0530

    Initial Template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello from React + TypeScript + Webpack ⚛️</h1>
+      <p>Everything works perfectly!</p>
+    </div>
+  );
+};
+
+export default App;
+ 
